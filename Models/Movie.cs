7e9f516b9c3a8052b1d6ba3dedd1d5476154581a
Rafael Adl Zarabi <rafaelzarabi@gmail.com>
2026-08-20 14:34:36 +0200

    Movie-klasse med title,duration og genre movieviewmodel med en movie property

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,6 +6,8 @@ namespace The_Movie_Gruppe_12.Models
 {
     public class Movie
     {
-
+        public string Title { get; set; }
+        public int Duration {get; set;  }
+        public string Genre { get; set; }
     }
 }
--- ViewModels/MovieViewModel.cs
+++ ViewModels/MovieViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using The_Movie_Gruppe_12.Models;
 
 namespace The_Movie_Gruppe_12.ViewModels
 {
    public class MovieViewModel
     {
-
+        public Movie Movie { get; set; } = new Movie();
     }
 }
